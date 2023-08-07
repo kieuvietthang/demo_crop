d40b33f11d2348ec2ad67468b793d970669d8a02
unknown <kieuvietthang>
2023-08-07 17:43:46 +0700

    fix code

--- lib/image_cropper.dart
+++ lib/image_cropper.dart
@@ -1,14 +1,18 @@
 import 'dart:async';
+import 'dart:developer';
 import 'dart:io';
-import 'dart:js_util';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
+import 'package:file_saver/file_saver.dart';
 import 'package:flutter/material.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:untitled/custom_clipper.dart';
+import 'package:untitled/custom_painter.dart';
 
 class MyHomePage extends StatefulWidget {
   final File imageFile;
+
   const MyHomePage({super.key, required this.imageFile});
 
   @override
@@ -16,16 +20,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Offset> pointList = [];
+  List<Offset> pointsList = [];
   bool cropImage = false;
   final ScreenshotController _screenshotController = ScreenshotController();
   late ui.Image image;
   bool isImageLoaded = false;
   int rotation = 0;
-  Future<void> init() async{
+
+  Future<void> init() async {
     image = await loadImage(File(widget.imageFile.path).readAsBytesSync());
   }
-
 
   Future<ui.Image> loadImage(Uint8List img) async {
     final Completer<ui.Image> completer = new Completer();
@@ -38,20 +42,123 @@ class _MyHomePageState extends State<MyHomePage> {
     return completer.future;
   }
 
-  Widget _buildImage(){
-    if (isImageLoaded){
-      return Center( )
+  Widget _buildImage() {
+    if (isImageLoaded) {
+      return Center(
+        child: RotatedBox(
+          quarterTurns: rotation,
+          child: FittedBox(
+            child: SizedBox(
+              height: image.height.toDouble(),
+              width: image.width.toDouble(),
+              child: cropImage
+                  ? Screenshot(
+                      controller: _screenshotController,
+                      child: ClipPath(
+                        clipper: MyClipper(
+                          pointsList: pointsList,
+                        ),
+                        child: Image.file(widget.imageFile),
+                      ))
+                  : CustomPaint(
+                      painter: MyImagePainter(
+                        pointsList,
+                        crop: cropImage,
+                        context: context,
+                        image: image,
+                        height: MediaQuery.of(context).size.width.toInt(),
+                      ),
+                      child: GestureDetector(
+                        onPanUpdate: (details) {
+                          Offset click = Offset(details.localPosition.dx,
+                              details.localPosition.dy);
+                          setState(() {
+                            if (click.dx > 0 &&
+                                click.dx < image.width &&
+                                click.dy > 0 &&
+                                click.dy < image.height) {
+                              pointsList.add(click);
+                            }
+                          });
+                        },
+                      ),
+                    ),
+            ),
+          ),
+        ),
+      );
     }
+    return Center(
+      child: Text('loading'),
+    );
   }
 
+  List<Widget> _iconDecider() {
+    if (isImageLoaded && !cropImage) {
+      return [
+        IconButton(
+          onPressed: () async {
+            setState(() {
+              cropImage = true;
+            });
+            _screenshotController.capture().then((Uint8List? imageList) async {
+              if (imageList != null) {
+                String? path = await FileSaver.instance.saveAs(
+                    name: 'result image',
+                    ext: 'png',
+                    mimeType: MimeType.png,
+                    bytes: imageList);
+                log(path!);
+              }
+            });
+          },
+          icon: Icon(Icons.edit),
+        ),
+      ];
+    }
+    return [];
+  }
+
+  Widget rotate() {
+    return BottomAppBar(
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () async {
+              setState(() {
+                rotation--;
+              });
+            },
+            icon: const Icon(Icons.rotate_left),
+          ),
+          IconButton(
+            onPressed: () async {
+              setState(() {
+                rotation++;
+              });
+            },
+            icon: const Icon(Icons.rotate_right),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   void initState() {
     init();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Doc Scanner"),
+        actions: _iconDecider(),
+      ),
+      body: _buildImage(),
+      bottomNavigationBar: rotate(),
+    );
   }
 }
